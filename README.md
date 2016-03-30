1e290146c19fa1f23b05f88956a270deae02d2e5
cncgl <concigel@gmail.com>
2016-03-31 00:30:13 +0900

    add problem1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scala-euler
-暇つぶし用 Project Euler
+暇つぶし用 [Project Euler](http://odz.sakura.ne.jp/projecteuler/index.php?Project%20Euler)
+
+## License
+[MIT](LICENSE)
--- src/main/scala/com/example/Problem1.scala
+++ src/main/scala/com/example/Problem1.scala
@@ -5,6 +5,7 @@ package com.example
   */
 object Problem1 {
   def main(args: Array[String]): Unit = {
-
+    println(Range(1,10).filter(x => (x % 5 == 0)||(x % 3 == 0)).sum)
+    println(Range(1,1000).filter(x => (x % 5 == 0)||(x % 3 == 0)).sum)
   }
 }
